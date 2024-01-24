49b111260ad460d8e678fc3bbf4c994d57bac605
Gresdian <chris_da98@outlook.it>
2024-01-24 16:47:21 +0100

    exercise finished

--- badWords.php
+++ badWords.php
@@ -1,11 +1,27 @@
+<?php
+$paragraph = $_GET['paragraph'];
+$badword = $_GET['badword'];
+
+$censored = str_replace($badword, '***', $paragraph)
+    ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <h1 class="text-center m-4">Testo originale</h1>
+    <p class="text-center m-4"><?php echo $paragraph; ?></p>
+    <p class="text-center">Lunghezza testo: <?php echo strlen($paragraph); ?></p>
+    <h1 class="text-center m-4">Testo censurato</h1>
+    <p class="text-center m-4"><?php echo $censored; ?></p>
+    <p class="text-center">Lunghezza testo censurato: <?php echo strlen($censored); ?></p>
 </body>
+
 </html>
--- index.php
+++ index.php
@@ -4,8 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    
+    <form action="./badWords.php" method="GET">
+        <textarea class="form-control" placeholder="Inserisci testo" name="paragraph" id="paragraph" cols="30" rows="10" required></textarea>
+        <div class="m-3">
+            <input type="text" class="form-control" name="badword" id="badword" placeholder="Inserisci parola da oscurare" required>
+        </div> 
+        <button class="btn btn-warning mx-3" type="submit">Invia</button>  
+    </form>
 </body>
 </html>
